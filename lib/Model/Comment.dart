66cef1579bbf20b31dd1bc6e4878abf8d54c13ae
enyason <enyasonjnr@yahoo.com>
2019-04-01 15:09:32 +0100

    edited comment class to include a name constructor

--- lib/Model/Comment.dart
+++ lib/Model/Comment.dart
@@ -1,22 +1,23 @@
-
-
-class Comment{
-
-  String commentBody,commentId,userId,timeStamp;
+class Comment {
+  String commentBody, commentId, userId, timeStamp;
 
   Comment({this.commentBody, this.commentId, this.userId, this.timeStamp});
 
-
-
-  Map toMap(){
-
-    return {
-      "comment_body":commentBody,
-      "comment_id" : commentId,
-      "time_stamp" : timeStamp,
-      "user_id" : userId
-    };
+  factory Comment.fromMap(Map comment) {
+    return Comment(
+      commentBody: comment["comment_body"],
+      commentId: comment["comment_id"],
+      timeStamp: comment["time_stamp"],
+      userId: comment["user_id"],
+    );
   }
 
-
+  Map<String, dynamic> toMap() {
+    return {
+      "comment_body": commentBody,
+      "comment_id": commentId,
+      "time_stamp": timeStamp,
+      "user_id": userId
+    };
+  }
 }
