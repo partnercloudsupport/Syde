9d0bf07473a45588063d957cdbd2ed184b79d088
enyason <enyasonjnr@yahoo.com>
2019-04-01 01:29:44 +0100

    refactor and code clean up for stories page

--- lib/BottomTabPages/page_stories.dart
+++ lib/BottomTabPages/page_stories.dart
@@ -10,39 +10,37 @@ class ThreadPage extends StatefulWidget {
   _ThreadPageState createState() => _ThreadPageState();
 
   const ThreadPage({Key key, this.user}) : super(key: key);
-  final FirebaseUser user;
+  final FirebaseUser user; // current user
 }
 
 class _ThreadPageState extends State<ThreadPage> {
-  bool isLiked = false;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder<QuerySnapshot>(
+        //listen to changes in the document reference
         stream: Firestore.instance
             .collection('all_post')
             .orderBy("time_stamp", descending: true)
             .snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> document) {
+          //return error message if there is an error
           if (document.hasError)
             return Center(child: Text('Error: ${document.error}'));
+
           switch (document.connectionState) {
+            //show progress dialog while state is waiting
             case ConnectionState.waiting:
               return Center(
                   child: CircularProgressIndicator(
                       valueColor: AlwaysStoppedAnimation<Color>(Colors.blue)));
             default:
+              //else return a listView : builds children lazily
               return ListView.builder(
                 itemCount: document.data.documents.length,
                 itemBuilder: (context, index) {
-//                  print(document.data.documents[index].data["user"]["user_photo"] + "?height=500");
+//
+                  // return each item at current position
                   return Padding(
                     padding: const EdgeInsets.only(left: 10.0, right: 10.0),
                     child: Card(
@@ -95,7 +93,8 @@ class _ThreadPageState extends State<ThreadPage> {
                           InkWell(
                             onTap: () {
                               //
-                              Story storyDetails = Story.fromMap(document.data.documents[index].data);
+                              Story storyDetails = Story.fromMap(
+                                  document.data.documents[index].data);
                               Navigator.push(context, MaterialPageRoute(
                                   builder: (BuildContext context) {
                                 return DetailedScreen(
@@ -146,13 +145,6 @@ class _ThreadPageState extends State<ThreadPage> {
                                         likePost(index, document);
                                         print("data is a not map $s");
                                       }
-//                                      print("valUE = ${dataSnapShot.data["like"]}");
-
-//                                      if(s["like"] == null){
-//                                        likePost(index, document);
-//                                        return;
-//                                      }
-//
 //
                                     });
                                   },
@@ -181,8 +173,6 @@ class _ThreadPageState extends State<ThreadPage> {
 
                                               DocumentSnapshot s =
                                                   snapshot.data ?? ["like"];
-//
-                                              print("val = ${s.data}");
 
                                               if (s.data == null) {
                                                 return Icon(
@@ -207,9 +197,6 @@ class _ThreadPageState extends State<ThreadPage> {
                                                   size: 18.0,
                                                 );
 
-//                                              print(
-//                                                  "like STREAM = ${snapshot.data["like"]}");
-//                                              return Text("hello");
                                             }),
                                         Text(
                                           "${document.data.documents[index]["like_count"]}",
@@ -239,25 +226,6 @@ class _ThreadPageState extends State<ThreadPage> {
                                       ],
                                     ),
                                   )),
-//                              InkWell(
-//                                  onTap: () {
-//                                    print("tapped!");
-//                                  },
-//                                  child: Padding(
-//                                    padding: const EdgeInsets.only(right: 20.0),
-//                                    child: Row(
-//                                      children: <Widget>[
-//                                        Icon(Icons.favorite_border,
-//                                            color: Colors.grey, size: 18.0),
-//                                        Text(
-//                                          "50k",
-//                                          style: TextStyle(
-//                                              color: Colors.grey,
-//                                              fontSize: 10.0),
-//                                        )
-//                                      ],
-//                                    ),
-//                                  )),
                             ],
                           ),
                           SizedBox(
@@ -274,64 +242,52 @@ class _ThreadPageState extends State<ThreadPage> {
                     )),
                   );
                 },
-//                reverse: false,
-//                children:
-//                    snapshot.data.documents.map((DocumentSnapshot document) {
-//
-//                }).toList(),
+
               );
           }
         },
       ),
 
-//        floatingActionButton: fab()
     );
   }
 
+  // this method handles liking a psot
   likePost(int index, AsyncSnapshot<QuerySnapshot> document) async {
+    //use transaction on the fire store instance to prevent race condition
     Firestore.instance.runTransaction((transaction) async {
+      //get a document snapshot at the current position
       DocumentSnapshot snapshot =
           await transaction.get(document.data.documents[index].reference);
 
+      //include uid to the like collection of the story
+      // and set "like" field to true
       await transaction.set(
           snapshot.reference.collection("likes").document(widget.user.uid),
           {"like": true});
 
+      //increment like count for the story
       await transaction.update(snapshot.reference,
           {"like_count": document.data.documents[index]["like_count"] + 1});
     });
   }
 
+  //this method handles unlike a post
   unLikePost(int index, AsyncSnapshot<QuerySnapshot> document) async {
     Firestore.instance.runTransaction((transaction) async {
       DocumentSnapshot snapshot =
           await transaction.get(document.data.documents[index].reference);
 
+      //unlike only when like count is > 0
       if (document.data.documents[index]["like_count"] > 0) {
+        // and set "like" field to false
         await transaction.set(
             snapshot.reference.collection("likes").document(widget.user.uid),
             {"like": false});
 
+        // decrement like count
         await transaction.update(snapshot.reference,
             {"like_count": document.data.documents[index]["like_count"] - 1});
       }
     });
-  }
-}
-
-class Fab extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-      heroTag: "home",
-      onPressed: () {
-        print("post here");
-        Navigator.push(context,
-            MaterialPageRoute(builder: (BuildContext context) {
-          return ThreadPost();
-        }));
-      },
-      child: Icon(Icons.add_box),
-    );
   }
 }
