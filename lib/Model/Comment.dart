fa689ff401621f71bca21f28b42029352dc36446
enyason <enyasonjnr@yahoo.com>
2019-04-01 13:56:02 +0100

    updated comment class to return a map

--- lib/Model/Comment.dart
+++ lib/Model/Comment.dart
@@ -4,7 +4,19 @@ class Comment{
 
   String commentBody,commentId,userId,timeStamp;
 
-  Comment(this.commentBody, this.commentId, this.userId, this.timeStamp);
+  Comment({this.commentBody, this.commentId, this.userId, this.timeStamp});
+
+
+
+  Map toMap(){
+
+    return {
+      "comment_body":commentBody,
+      "comment_id" : commentId,
+      "time_stamp" : timeStamp,
+      "user_id" : userId
+    };
+  }
 
 
 }
