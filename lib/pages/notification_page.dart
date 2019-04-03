e4b5ba4c345acc26ead94b4dbd10162719aeb9b5
enyason <enyasonjnr@yahoo.com>
2019-04-03 13:48:17 +0100

    implemented a basic structure of the notification page connected, to the sever

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NotificationScreen extends StatefulWidget {
   @override
@@ -8,14 +9,37 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+        body: StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance.collection("announcement").snapshots(),
+            builder: (context, snapshot) {
+              print(snapshot.data.documents[0]["title"]);
 
-      body: Container(
-        child: Center(child: Text("Notifications not available!")),
+              return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    return makeNotificationItem(
+                        snapshot.data.documents[index].data);
+                  });
+            }));
+  }
+
+  Widget makeNotificationItem(Map data) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            data["title"],
+            style: TextStyle(fontSize: 18.0),
+          ),
+          Text(
+            data["body"],
+            style: TextStyle(fontSize: 14.0),
+          )
+        ],
       ),
-
     );
-
   }
 }
