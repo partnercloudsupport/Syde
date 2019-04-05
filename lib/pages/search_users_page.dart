5e25ec7fc398e3205d055a810c6572305e6074ad
enyason <enyasonjnr@yahoo.com>
2019-04-05 16:01:08 +0100

    connected page to server to fetch and display users on the platform

--- lib/pages/search_users_page.dart
+++ lib/pages/search_users_page.dart
@@ -1,9 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pk_skeleton/pk_skeleton.dart';
-
-
 
 class SearchUsers extends StatefulWidget {
   @override
@@ -15,30 +12,51 @@ class _SearchUsersState extends State<SearchUsers> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
-        leading: Icon(Icons.arrow_back,color: Colors.white,),
-        title: Text("New Messages",style: TextStyle(color: Colors.white),),
+        leading: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+        title: Text(
+          "New Messages",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-
-      body: StreamBuilder(
+      body: StreamBuilder<QuerySnapshot>(
           stream: Firestore.instance.collection("users").snapshots(),
-          builder: (context,snapshot){
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting)
+              return PKCardListSkeleton(
+                isCircularImage: true,
+                isBottomLinesActive: true,
+                length: 10,
+              );
 
-            print(snapshot.data["name"]);
-            if(snapshot.connectionState == ConnectionState.waiting) return PKCardListSkeleton(
-                    isCircularImage: true,
-                    isBottomLinesActive: true,
-                    length: 10,
-                  );
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data.documentChanges.length,
+                  itemBuilder: (context, index) {
+//                    return Text("Hey ${snapshot.data.documents[index]["name"]}");
 
-
-
-            return ListView.builder(itemBuilder: (context,index){
-
-              return Text("Hey $index");
-            });
-      }),
-
+                    return ListTile(
+                      onTap: () {},
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            snapshot.data.documents[index]["photoUrl"] +
+                                "?height=500"),
+                      ),
+                      title: Text("${snapshot.data.documents[index]["name"]}"),
+                      subtitle:
+                          Text("${snapshot.data.documents[index]["email"]}"),
+                    );
+                  });
+            } else {
+              return Center(
+                child: Text("No user found!"),
+              );
+            }
+          }),
     );
   }
+
+  Widget makeUserItem() {}
 }
