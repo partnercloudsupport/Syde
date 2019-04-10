61cacdf27363295f4ebf33c62d9e1235fc900d46
enyason <enyasonjnr@yahoo.com>
2019-04-10 22:42:10 +0100

    gave appbar a title

--- lib/pages/detailed_page.dart
+++ lib/pages/detailed_page.dart
@@ -16,13 +16,9 @@ class DetailedScreen extends StatefulWidget {
 }
 
 class _DetailedScreenState extends State<DetailedScreen> {
-  String textBody =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus elementum sagittis vitae et leo. Purus semper eget duis at tellus at urna condimentum mattis. Sed enim ut sem viverra aliquet eget. Ac turpis egestas sed tempus urna et pharetra pharetra. Non tellus orci ac auctor augue mauris augue. Volutpat odio facilisis mauris sit amet massa vitae. Aenean vel elit scelerisque mauris. Rutrum tellus pellentesque eu tincidunt tortor. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor. Etiam non quam lacus suspendisse faucibus interdum. Fusce id velit ut tortor pretium viverra suspendisse potenti. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Laoreet sit amet cursus sit amet dictum. Aliquet nibh praesent tristique magna. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Lacinia quis vel eros donec ac odio. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus. Lectus proin nibh nisl condimentum id. Habitant morbi tristique senectus et netus et. Eros in cursus turpis massa. Aliquet enim tortor at auctor urna nunc id cursus. Lectus arcu bibendum at varius vel pharetra vel turpis. Tellus integer feugiat scelerisque varius morbi enim nunc. Eget sit amet tellus cras adipiscing enim eu. Risus sed vulputate odio ut enim blandit. Viverra justo nec ultrices dui sapien eget mi proin. Natoque penatibus et magnis dis parturient montes. Aliquet lectus proin nibh nisl condimentum. Massa placerat duis ultricies lacus sed turpis tincidunt. Viverra nam libero justo laoreet sit. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies. Integer eget aliquet nibh praesent tristique. Ultricies leo integer malesuada nunc vel.";
-
   CollectionReference _commentRef;
   TextEditingController _controller = TextEditingController();
   FirebaseUser _user;
-
 
   @override
   void initState() {
@@ -35,8 +31,7 @@ class _DetailedScreenState extends State<DetailedScreen> {
     super.initState();
   }
 
-  getUser() async{
-
+  getUser() async {
     _user = await FirebaseAuth.instance.currentUser();
   }
 
@@ -50,13 +45,15 @@ class _DetailedScreenState extends State<DetailedScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: widget.story.storyTitle == null
+            ? Text("")
+            : Text(widget.story.storyTitle),
         elevation: 0.0,
         backgroundColor: Colors.blue,
         leading: IconButton(
             icon: Icon(
               Icons.arrow_back,
               color: Colors.white,
-
             ),
             onPressed: () {
               Navigator.pop(context);
@@ -84,19 +81,21 @@ class _DetailedScreenState extends State<DetailedScreen> {
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
             ),
           ),
-
           Padding(
-            padding: const EdgeInsets.only(left:8.0,right: 8.0,bottom: 4.0,top: 8.0),
-            child: Divider(height: 1.0,color: Colors.grey,),
+            padding: const EdgeInsets.only(
+                left: 8.0, right: 8.0, bottom: 4.0, top: 8.0),
+            child: Divider(
+              height: 1.0,
+              color: Colors.grey,
+            ),
           ),
           Padding(
-              padding:
-                  const EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0,right: 16.0),
+              padding: const EdgeInsets.only(
+                  left: 16.0, top: 8.0, bottom: 8.0, right: 16.0),
               child: Row(
 //
                   children: <Widget>[
                     CircleAvatar(
-
                       radius: 20.0,
                       backgroundImage: NetworkImage(
                           widget.story.user["user_photo"] + "?height=500"),
@@ -124,16 +123,17 @@ class _DetailedScreenState extends State<DetailedScreen> {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Text(widget.story.storyBody,
-                style:
-                    TextStyle(fontSize: 14.0, color: Colors.black, height: 2.0)),
+                style: TextStyle(
+                    fontSize: 14.0, color: Colors.black, height: 2.0)),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 16.0,right: 16.0),
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
             child: StreamBuilder<QuerySnapshot>(
-              stream: _commentRef.orderBy("time_stamp", descending: true).snapshots(),
-              builder: (context,AsyncSnapshot<QuerySnapshot> snapshot){
-
-                if(snapshot.connectionState == ConnectionState.waiting)
+              stream: _commentRef
+                  .orderBy("time_stamp", descending: true)
+                  .snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting)
 //                  return PKCardListSkeleton(
 //                    isCircularImage: true,
 //                    isBottomLinesActive: true,
@@ -141,30 +141,24 @@ class _DetailedScreenState extends State<DetailedScreen> {
 //                  );
 
                   return Center(
-                  child: Container(
-                    width: 50.0,
-                    height: 50.0,
-
-
-                    child: CircularProgressIndicator(
-                      strokeWidth: 2,
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.black)),
-
-
-                  ),
-                );
-
-                if(snapshot.data == null){
-                  print("snapshot is null");
-                  return Container(
-
-                    child: Center(child: Text("No comments yet!")),
+                    child: Container(
+                      width: 50.0,
+                      height: 50.0,
+                      child: CircularProgressIndicator(
+                          strokeWidth: 2,
+                          valueColor:
+                          AlwaysStoppedAnimation<Color>(Colors.black)),
+                    ),
                   );
 
+                if (snapshot.data == null) {
+                  print("snapshot is null");
+                  return Container(
+                    child: Center(child: Text("No comments yet!")),
+                  );
                 }
 
-                if(snapshot.data.documents.length <1){
-
+                if (snapshot.data.documents.length < 1) {
                   return Container(
                     child: Center(child: Text("No comments yet!")),
                   );
@@ -175,14 +169,9 @@ class _DetailedScreenState extends State<DetailedScreen> {
                     physics: ClampingScrollPhysics(),
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
-
-                      return _buildCommentItem(snapshot,index);
+                      return _buildCommentItem(snapshot, index);
                     });
-
-
-
               },
-
             ),
           )
         ],
@@ -218,14 +207,17 @@ class _DetailedScreenState extends State<DetailedScreen> {
                 ),
                 FlatButton(
                     onPressed: () {
-
                       print(_controller.text.length);
-                      if(_controller.text.trim().length<1)return;
+                      if (_controller.text
+                          .trim()
+                          .length < 1) return;
 
                       addComment();
-
                     },
-                    child: Icon(Icons.send,color: Colors.blue,))
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.blue,
+                    ))
               ],
             ),
           ),
@@ -235,30 +227,40 @@ class _DetailedScreenState extends State<DetailedScreen> {
   }
 
   Widget _buildCommentItem(AsyncSnapshot<QuerySnapshot> snapshot, int index) {
-
     Comment comment = Comment.fromMap(snapshot.data.documents[index].data);
 
     return StreamBuilder(
-      stream: Firestore.instance.collection("users").document(comment.userId).snapshots(),
-        builder: (context,userSnapshot) {
+        stream: Firestore.instance
+            .collection("users")
+            .document(comment.userId)
+            .snapshots(),
+        builder: (context, userSnapshot) {
           if (userSnapshot.connectionState == ConnectionState.waiting)
             return Container(
-              child: Center(child: PKCardSkeleton(
-                isCircularImage: true,
-                isBottomLinesActive: true,
-              ),),);
+              child: Center(
+                child: PKCardSkeleton(
+                  isCircularImage: true,
+                  isBottomLinesActive: true,
+                ),
+              ),
+            );
 
           print("shot ${userSnapshot.data["name"]}");
 
-          if (userSnapshot.data == null) return Container(color: Colors.white,);
-          else  return makeItem(comment, userSnapshot);
-
+          if (userSnapshot.data == null)
+            return Container(
+              color: Colors.white,
+            );
+          else
+            return makeItem(comment, userSnapshot);
         });
   }
-    Widget makeItem(Comment comment, AsyncSnapshot userSnapshot){
-      DateTime date =  DateTime.fromMillisecondsSinceEpoch(int.parse(comment.timeStamp));
-      var format =  DateFormat("yMMMMd");
-      var dateString = format.format(date);
+
+  Widget makeItem(Comment comment, AsyncSnapshot userSnapshot) {
+    DateTime date =
+    DateTime.fromMillisecondsSinceEpoch(int.parse(comment.timeStamp));
+    var format = DateFormat("yMMMMd");
+    var dateString = format.format(date);
     return Padding(
       padding: const EdgeInsets.only(bottom: 10.0),
       child: Row(
@@ -270,10 +272,9 @@ class _DetailedScreenState extends State<DetailedScreen> {
             backgroundImage:
             NetworkImage(userSnapshot.data["photoUrl"] + "?height=500"),
           ),
-
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.only(top:10.0,left: 10.0),
+              padding: const EdgeInsets.only(top: 10.0, left: 10.0),
               child: Column(
                 children: <Widget>[
 //                SizedBox(height: 40.0,),
@@ -287,7 +288,7 @@ class _DetailedScreenState extends State<DetailedScreen> {
                           style: TextStyle(
                               color: Colors.black, fontWeight: FontWeight.w500),
                         ),
-                        Text(dateString                          ,
+                        Text(dateString,
                             style: TextStyle(
                                 color: Colors.grey,
                                 fontWeight: FontWeight.w300,
@@ -295,19 +296,15 @@ class _DetailedScreenState extends State<DetailedScreen> {
                       ],
                     ),
                   ),
-                  SizedBox(height:10.0),
+                  SizedBox(height: 10.0),
                   Align(
                     alignment: Alignment.bottomLeft,
-                    child: Text(
-
-                      comment.commentBody,
+                    child: Text(comment.commentBody,
                         style: TextStyle(
                             color: Colors.black,
                             fontWeight: FontWeight.normal,
                             fontSize: 12.0)),
                   ),
-
-
                 ],
               ),
             ),
@@ -317,48 +314,37 @@ class _DetailedScreenState extends State<DetailedScreen> {
     );
   }
 
-
-
-  addComment(){
-
+  addComment() {
     Map<String, dynamic> data = Comment(
         commentBody: _controller.value.text,
         commentId: "na",
         userId: _user.uid,
-        timeStamp: DateTime.now()
+        timeStamp: DateTime
+            .now()
             .millisecondsSinceEpoch
             .toString())
         .toMap();
 
     _controller.clear();
     _commentRef.add(data).then((docRef) {
-
-
       _commentRef
           .document(docRef.documentID)
           .updateData({"comment_id": docRef.documentID});
 
       incrementComment(widget.story.storyId);
-
-
-
-    }
-
-    );
-
+    });
   }
 
   incrementComment(String docRef) async {
     //use transaction on the fire store instance to prevent race condition
     Firestore.instance.runTransaction((transaction) async {
       //get a document snapshot at the current position
-      DocumentSnapshot snapshot =
-      await transaction.get(Firestore.instance.collection("all_post").document(docRef));
+      DocumentSnapshot snapshot = await transaction
+          .get(Firestore.instance.collection("all_post").document(docRef));
 
       //increment comment count for the story
       await transaction.update(snapshot.reference,
           {"comment_count": snapshot.data["comment_count"] + 1});
-
     });
   }
 }
