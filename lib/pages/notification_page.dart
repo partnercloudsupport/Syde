0a9a1c3ab6b15f16dad9b1b422e93364de06e3d0
enyason <enyasonjnr@yahoo.com>
2019-04-10 21:34:57 +0100

    updated the UI of the notification list page

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class NotificationScreen extends StatefulWidget {
   @override
@@ -24,10 +25,12 @@ class _NotificationScreenState extends State<NotificationScreen> {
                 Navigator.pop(context);
               }),
         ),
-        body: FutureBuilder(
+        body: FutureBuilder( //future builder is used to retrieve data from backend
             future:
-            Firestore.instance.collection("announcement").getDocuments(),
+            Firestore.instance.collection("announcement").orderBy(
+                "index", descending: true).getDocuments(),
             builder: (context, snapshot) {
+              //display a circle progress bar while connection state is waiting
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return Center(
                   child: CircularProgressIndicator(
@@ -36,6 +39,12 @@ class _NotificationScreenState extends State<NotificationScreen> {
                 );
               }
 
+              //in a case of no data, display a text showing "not available"
+              if (snapshot.data == null) {
+                return Center(child: Text("Not Available"),);
+              }
+
+              //display a list of items when other conditions stated above are not met
               return ListView.builder(
                   itemCount: snapshot.data.documents.length,
                   itemBuilder: (context, index) {
@@ -45,21 +54,45 @@ class _NotificationScreenState extends State<NotificationScreen> {
             }));
   }
 
+  //make widget for each notification item
   Widget makeNotificationItem(Map data) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            data["title"],
-            style: TextStyle(fontSize: 18.0),
-          ),
-          Text(
-            data["body"],
-            style: TextStyle(fontSize: 14.0),
-          )
-        ],
+    return InkWell(
+      onTap: () {
+
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(
+            top: 8.0, bottom: 8.0, left: 16.0, right: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    data["title"],
+                    style: TextStyle(fontSize: 14.0,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+
+                Text(
+                  data["date"],
+                  style: TextStyle(fontSize: 10.0,),
+                ),
+              ],
+            ),
+
+            Text(
+              data["body"].toString().trim(),
+              maxLines: 2,
+              style: TextStyle(fontSize: 14.0),
+            )
+          ],
+        ),
       ),
     );
   }
